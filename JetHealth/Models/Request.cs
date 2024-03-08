8e601ee41d1b7e12006e0174ba8fa1295047f4c6
Olexander Pylypchuk <sashylichka2016@gmail.com>
2024-03-08 16:02:25 +0200

    Added Class Diagram and ApplicationUser

--- JetHealth/Models/Request.cs
+++ JetHealth/Models/Request.cs
@@ -1,6 +1,8 @@
-﻿namespace JetHealth.Models
+﻿using JetHealth.Models.Abstract;
+
+namespace JetHealth.Models
 {
-    public class Request
+    public class Request:Message
     {
         public string Name { get; set; }
         public string PhoneNumber { get; set; }
